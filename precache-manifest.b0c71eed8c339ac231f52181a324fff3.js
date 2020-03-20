c76e552cf6ecd0eb73be3689407690fd9a56daee
Klaudia <klaudia.krzywda@gmail.com>
2020-03-20 11:29:30 +0000

    Updates

---
+++ precache-manifest.b0c71eed8c339ac231f52181a324fff3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cf0d9d1ce7ea208eb199a3fa3a583a5",
+    "url": "/Compare-product/index.html"
+  },
+  {
+    "revision": "a5d1560e3050e4522a37",
+    "url": "/Compare-product/static/css/main.489f1cce.chunk.css"
+  },
+  {
+    "revision": "2f492a76bdf7514cea97",
+    "url": "/Compare-product/static/js/2.eb40492a.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Compare-product/static/js/2.eb40492a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5d1560e3050e4522a37",
+    "url": "/Compare-product/static/js/main.99a366d7.chunk.js"
+  },
+  {
+    "revision": "6a235f2b0a68069c93a7",
+    "url": "/Compare-product/static/js/runtime-main.693631dd.js"
+  },
+  {
+    "revision": "b0461f21e2ad2149b9bf45e51f1215e4",
+    "url": "/Compare-product/static/media/apple.b0461f21.png"
+  },
+  {
+    "revision": "103cc15089d2949f016758a304880248",
+    "url": "/Compare-product/static/media/kiwi.103cc150.png"
+  },
+  {
+    "revision": "a3f96b85d302cf3d4f375bbd105fddec",
+    "url": "/Compare-product/static/media/mango.a3f96b85.png"
+  },
+  {
+    "revision": "2e9f99aa131c566b591fc002fe96bb29",
+    "url": "/Compare-product/static/media/orange.2e9f99aa.png"
+  }
+]);
